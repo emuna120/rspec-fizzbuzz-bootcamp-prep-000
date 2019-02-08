8a090a2d8cde2ba90bf3a665ac5d4904ce076614
Amy Man <jwb74fko3ovz@beconfidential.com>
2019-02-08 05:50:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 3 == 0 && int % 5 == 0)
-    return "fizzbuzz"
+    return "FizzBuzz"
   elsif int % 3 == 0
-    return ""
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end  
 end  
